c132f5536a5aac5ed1c87a1995f84233f8ef5f9c
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-07-25 18:36:59 -0400

    feat: add about section (#3)

---
+++ components/Home/About.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Logo from "../../public/logo.png"
+import { eventTimeShort } from '../../util/config'
+
+export default function About() {
+    return (
+        <div className="flex p-10 flex-col items-center lg:flex-row lg:p-20 xl:px-40 items-left bg-emerald-800 gap-6 z-[10]">
+            <div className="flex flex-col items-center lg:items-start w-4/5 text-center lg:text-left mb-4 lg:mb-0">
+                <h1 className="text-white font-bold text-2xl xs:text-3xl md:text-4xl">Canada&apos;s first <span className='text-green-300'>green-centric</span> hackathon</h1>
+                <p className="mt-4 w-full md:w-3/4 text-lg text-gray-200">
+                    TurtleHacks is a green-centric hackathon happening from {eventTimeShort}, 
+                    where hackers all over the world can gather for a weekend of learning and creating solutions to help the environment 
+                    through code.
+                </p>
+
+                <p className='mt-4 w-full md:w-3/4 text-lg text-gray-200'>
+                    During this 36-hour hackathon, you can work in teams of up to four to create anyhing with one goal in mind: helping the 
+                    environment. After finishing your project, you can pitch your project to a panel of judges to compete for prizes!  
+                </p>
+            </div>
+
+            <div className="flex p-0 m-0 w-1/2 md:w-1/4">
+                <div className='flex ring-8 ring-emerald-500 rounded-full'>
+                    <Image className="rounded-full" src={Logo} alt="Logo" objectFit="cover" objectPosition="center" width={540} height={540} quality={100} placeholder="blur" />
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Layout from "../components/Layout"
 import Hero from "../components/Home/Hero"
+import About from "../components/Home/About"
 
 export default function Home() {
   return (
     <Layout name="Home" noAnim home>
       <Hero />
+      <About />
     </Layout>
   )
 }

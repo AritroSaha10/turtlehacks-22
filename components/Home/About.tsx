a8cf4e6e26d4c2826f36acb2b4ec4b9d761b8cc4
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-07-25 20:28:14 -0400

    feat: make wave static (#2)

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -7,9 +7,9 @@ import Logo from "../../public/logo.png"
 
 export default function About() {
     return (
-        <div className="flex p-10 flex-col items-center lg:flex-row lg:p-20 xl:px-40 items-left bg-sky-800 gap-6 z-[30]">
+        <div className="flex p-10 flex-col items-center lg:flex-row lg:p-20 lg:py-28 xl:px-40 items-left bg-sky-800 gap-6 z-[30] pt-0 lg:pt-8">
             <div className="flex flex-col items-center lg:items-start w-4/5 text-center lg:text-left mb-4 lg:mb-0">
-                <h1 className="text-white font-bold text-2xl xs:text-3xl md:text-4xl">Canada&apos;s first <span className='text-green-300'>green-centric</span> hackathon</h1>
+                <h1 className="text-white font-bold text-3xl md:text-4xl">Canada&apos;s first <span className='text-green-300'>green-centric</span> hackathon</h1>
                 <p className="mt-4 w-full md:w-3/4 text-lg text-gray-200">
                     TurtleHacks is a green-centric hackathon happening from {eventTimeShort},
                     where hackers all over the world can gather for a weekend of learning and creating solutions to help the environment
--- components/Home/WaveEffect.tsx
+++ components/Home/WaveEffect.tsx
@@ -1,38 +1,23 @@
-import { useState } from 'react';
-import { useScrollPosition } from '@n8tb1t/use-scroll-position'
-
-const clamp = (num: number, min: number, max: number) => Math.min(Math.max(num, min), max);
+const topWavePath = "M 0,600 C 0,600 0,200 0,200 C 101.39285714285714,193.03571428571428 202.78571428571428,186.07142857142858 313,196 C 423.2142857142857,205.92857142857142 542.25,232.75 660,232 C 777.75,231.25 894.2142857142858,202.92857142857144 1024,193 C 1153.7857142857142,183.07142857142856 1296.892857142857,191.53571428571428 1440,200 C 1440,200 1440,600 1440,600 Z";
+const bottomWavePath = "M 0,600 C 0,600 0,400 0,400 C 134.85714285714286,415.3571428571429 269.7142857142857,430.7142857142857 378,434 C 486.2857142857143,437.2857142857143 568.0000000000001,428.5 685,417 C 801.9999999999999,405.5 954.2857142857142,391.2857142857143 1086,388 C 1217.7142857142858,384.7142857142857 1328.857142857143,392.3571428571429 1440,400 C 1440,400 1440,600 1440,600 Z";
 
 export default function WaveEffect() {
-    const [waveHeight, setWaveHeight] = useState('0px');
-    const [bottomWaveHeight, setBottomWaveHeight] = useState('0px');
-
-    useScrollPosition(({ currPos }) => {
-        const currScroll = clamp(Math.abs(currPos.y), 0, 925);
-        let curveVal = currScroll / 925 * Math.PI;
-
-        const topHeight = Math.abs(Math.sin(curveVal) * 200);
-        const bottomHeight = Math.abs(Math.sin(curveVal) * 300);
-
-        setWaveHeight(`${topHeight}px`);
-        setBottomWaveHeight(`${bottomHeight}px`);
-    })
-
     return (
         <>
-            <div className='h-0 bottom-0 flex items-end'>
-                <svg preserveAspectRatio="none" width="100%" height={waveHeight} viewBox="0 0 754 144" fill="none" xmlns="http://www.w3.org/2000/svg" className='z-[20] relative'>
-                    <path d="M0 144V0C123 0 106.5 56.5 192 56.5C273.5 56.5 327.5 0 392 0C456.5 0 503.474 56.5 581.5 56.5C646 56.5 718.833 27.8333 754 0V144H0Z" fill="#075985">
+            <div className='h-0'>
+                <svg preserveAspectRatio="none" width="100%" height="300px" viewBox="0 0 1440 600" fill="none" xmlns="http://www.w3.org/2000/svg" className='z-[10] relative'>
+                    <path d={topWavePath} stroke="none" strokeWidth="0" fill="#009eb5" />
+                </svg>
+            </div>
+
+            <div className='h-0'>
+                <svg preserveAspectRatio="none" width="100%" height="300px" viewBox="0 0 1440 600" fill="none" xmlns="http://www.w3.org/2000/svg" className='z-[20] relative'>
+                    <path d={bottomWavePath} stroke="none" strokeWidth="0" fill="#075985">
                     </path>
                 </svg>
             </div>
 
-            <div className='h-0 bottom-0 flex items-end'>
-                <svg preserveAspectRatio="none" width="100%" height={bottomWaveHeight} viewBox="0 0 754 144" fill="none" xmlns="http://www.w3.org/2000/svg" className='z-10 relative'>
-                    <path d="M0 144V0C123 0 106.5 56.5 192 56.5C273.5 56.5 327.5 0 392 0C456.5 0 503.474 56.5 581.5 56.5C646 56.5 718.833 27.8333 754 0V144H0Z" fill="#009eb5">
-                    </path>
-                </svg>
-            </div>
+            <div className='h-[300px]' />
         </>
     )
 }

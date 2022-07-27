a156793da51cf559bd4e2b4a856971764e1f57ea
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-07-27 18:16:19 -0400

    fix: allow scrolling to about

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -7,7 +7,7 @@ import Logo from "@media/logo.png"
 
 export default function About() {
     return (
-        <section className="flex p-10 flex-col items-center lg:flex-row lg:p-20 xl:px-40 items-left bg-sky-800 gap-6 z-[30] py-0 lg:pt-8 pb-0 lg:pb-0 xl:pb-0">
+        <section className="flex p-10 flex-col items-center lg:flex-row lg:p-20 xl:px-40 items-left bg-sky-800 gap-6 z-[30] py-0 lg:pt-8 pb-0 lg:pb-0 xl:pb-0" id="about">
             <div className="flex flex-col items-center lg:items-start w-4/5 text-center lg:text-left mb-4 lg:mb-0">
                 <h1 className="text-white font-bold text-3xl md:text-4xl">Canada&apos;s first <span className='text-green-300'>green-centric</span> hackathon</h1>
                 <p className="mt-4 w-full md:w-3/4 text-lg text-gray-200">

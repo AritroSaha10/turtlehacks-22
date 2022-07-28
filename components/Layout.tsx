6598cb2e3456ce8667f68f199fa51419c2134f96
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-07-28 15:17:02 -0400

    fix: better icons

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -33,6 +33,11 @@ export default function Layout({ name, children, noAnim, home }: Props) {
             <Head>
                 <title>{title}</title>
                 
+                <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+ 
                 <meta name="description" content={description} />
 
                 <meta property="og:title" content={title} />

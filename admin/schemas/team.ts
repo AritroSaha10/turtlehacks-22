4ca2eb1c88d486a030653962644ef90f4cca7133
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-07-28 13:32:35 -0400

    fix: allow multiple links for person

--- admin/schemas/team.ts
+++ admin/schemas/team.ts
@@ -1,75 +1,99 @@
 import { MdPeople } from "react-icons/md";
 
 export default {
-  name: 'team',
-  title: 'Team',
-  type: 'document',
-  icon: MdPeople,
-  fields: [
-    {
-      name: "order",
-      title: "Order",
-      type: "number",
-      hidden: true,
+    name: 'team',
+    title: 'Team',
+    type: 'document',
+    icon: MdPeople,
+    fields: [
+        {
+            name: "order",
+            title: "Order",
+            type: "number",
+            hidden: true,
+        },
+        {
+            name: 'position',
+            title: 'Position',
+            type: 'string',
+            validation: (Rule: any) => Rule.required()
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+            validation: (Rule: any) => Rule.required()
+        },
+        {
+            title: 'Links',
+            name: 'links',
+            type: 'array',
+            of: [
+                {
+                    type: 'object',
+                    title: 'Link',
+                    name: 'link',
+                    fields: [
+                        {
+                            name: "website",
+                            type: "string",
+                            title: "Website",
+                            options: {
+                                list: [
+                                    "GitHub",
+                                    "Instagram",
+                                    "LinkedIn",
+                                    "Website",
+                                ],
+                                layout: 'radio' // <-- defaults to 'dropdown'
+                            }
+                        },
+                        { name: "url", type: "url", title: "URL", validation: (Rule: any) => Rule.required() },
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 96,
+            },
+            validation: (Rule: any) => Rule.required()
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+            validation: (Rule: any) => Rule.required()
+        },
+    ],
+    preview: {
+        select: {
+            title: 'name',
+            subtitle: 'position',
+            media: 'image',
+        },
     },
-    {
-      name: 'position',
-      title: 'Position',
-      type: 'string',
-      validation: (Rule: any) => Rule.required()
-    },
-    {
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-      validation: (Rule: any) => Rule.required()
-    },
-    {
-      name: 'website',
-      title: 'Website',
-      type: 'url',
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'name',
-        maxLength: 96,
-      },
-      validation: (Rule: any) => Rule.required()
-    },
-    {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-      validation: (Rule: any) => Rule.required()
-    },
-  ],
-  preview: {
-    select: {
-      title: 'name',
-      subtitle: 'position',
-      media: 'image',
-    },
-  },
-  orderings: [
-    {
-      title: 'Order',
-      name: 'order',
-      by: [
-        { field: 'order', direction: 'asc' }
-      ]
-    },
-    {
-      title: 'Name',
-      name: 'name',
-      by: [
-        { field: 'name', direction: 'asc' }
-      ]
-    },
-  ]
+    orderings: [
+        {
+            title: 'Order',
+            name: 'order',
+            by: [
+                { field: 'order', direction: 'asc' }
+            ]
+        },
+        {
+            title: 'Name',
+            name: 'name',
+            by: [
+                { field: 'name', direction: 'asc' }
+            ]
+        },
+    ]
 }

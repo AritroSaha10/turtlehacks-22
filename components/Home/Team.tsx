0de8d290488f9aaa9bb7ec065605506c8a2c85a0
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-07-31 19:20:25 -0400

    fix: team sect anims
    #11

--- components/Home/Team.tsx
+++ components/Home/Team.tsx
@@ -8,6 +8,23 @@ import { SanityReference } from "@sanity/image-url/lib/types/types";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { MdWeb } from "react-icons/md";
 
+import { m } from "framer-motion"
+import transition from "@util/anim/transition";
+
+const teamVariants = {
+    initial: {},
+    animate: {
+        transition: {
+            staggerChildren: 0.1
+        }
+    }
+};
+
+const memberVariants = {
+    initial: { opacity: 0, y: 60 },
+    animate: { opacity: 1, y: 0, transition: { ...transition, duration: 0.7 }, }
+};
+
 export interface TeamMemberData {
     position: string,
     name: string,
@@ -19,7 +36,7 @@ export interface TeamMemberData {
 };
 
 const iconMapping = {
-    "GitHub": <FaGithub className="text-xl text-white hover:text-gray-300 duration-150"/>,
+    "GitHub": <FaGithub className="text-xl text-white hover:text-gray-300 duration-150" />,
     "Instagram": <FaInstagram className="text-xl text-white hover:text-gray-300 duration-150" />,
     "LinkedIn": <FaLinkedin className="text-xl text-white hover:text-gray-300 duration-150" />,
     "Website": <MdWeb className="text-xl text-white hover:text-gray-300 duration-150" />
@@ -35,17 +52,22 @@ const TeamMember = ({ position, name, image, links }: TeamMemberData) => {
     links = links ?? [];
 
     return (
-        <div className="flex flex-col items-center text-center">
-            <Image
-                {...imageProps}
-                height={200}
-                width={200}
-                objectFit="cover"
-                objectPosition="center"
-                alt="Photo"
-                placeholder="blur"
-                className="rounded-full"
-            />
+        <m.div
+            className="flex flex-col items-center text-center relative"
+            variants={memberVariants}
+        >
+            <div className="rounded-full">
+                <Image
+                    {...imageProps}
+                    height={200}
+                    width={200}
+                    objectFit="cover"
+                    objectPosition="center"
+                    alt="Photo"
+                    placeholder="blur"
+                    className="rounded-full"
+                />
+            </div>
             <h1 className="text-3xl font-bold text-white mt-4">{name}</h1>
             <h1 className="text-xl font-semibold text-emerald-300 my-1">{position}</h1>
 
@@ -56,7 +78,7 @@ const TeamMember = ({ position, name, image, links }: TeamMemberData) => {
                     </a>
                 ))}
             </div>
-        </div>
+        </m.div>
     )
 }
 
@@ -68,11 +90,16 @@ export default function Team({ team }: { team: TeamMemberData[] }) {
                 <h1 className="text-4xl text-white font-bold">Team</h1>
             </div>
 
-            <div className="flex flex-wrap gap-8 lg:gap-16 justify-around w-full">
-                {team.map(data => (
-                    <TeamMember {...data} key={data.name} />
+            <m.div
+                className="flex flex-wrap gap-8 lg:gap-16 justify-around w-full"
+                variants={teamVariants}
+                initial="initial"
+                animate="animate"
+            >
+                {team.map((data, idx) => (
+                    <TeamMember {...data} key={idx} />
                 ))}
-            </div>
+            </m.div>
         </section>
     )
 }

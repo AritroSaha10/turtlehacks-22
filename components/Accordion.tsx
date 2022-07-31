d9066709a644596e80d393835f8e38ee2973b22a
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-07-30 23:57:43 -0400

    fix: remove material tailwind

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,9 +1,11 @@
 import { FC, useState } from "react";
+/*
 import {
     Accordion as MaterialAccordion,
     AccordionHeader,
     AccordionBody
 } from "@material-tailwind/react";
+*/
 
 interface Props {
     title: string,
@@ -18,12 +20,19 @@ const Accordion: FC<Props> = ({ title, content, keepSeparator }) => {
         setOpen(!open);
     };
 
+    /*
     return (
         <MaterialAccordion open={open} onClick={handleToggle} className="border-2 border-white/50 px-4 relative mt-[-2px]">
             <AccordionHeader className="text-white text-left">{title}</AccordionHeader>
             <AccordionBody className="text-white mt-[-20px]">{content}</AccordionBody>
         </MaterialAccordion>
     );
+    */
+
+    return (
+        <>
+        </>
+    )
 }
 
 export default Accordion;

1fa4884db7cbf344c3f0e0bda2e58ac1f035c284
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2022-10-25 20:33:36 -0400

    fix: add more accurate config details

--- util/config.ts
+++ util/config.ts
@@ -1,7 +1,7 @@
 const name = "TurtleHacks"
-const eventTimeShort = "February"
-const eventTime = "February 2023"
-const description = `TurtleHacks is a hackathon happening during ${eventTime} focused on the environment.`
+const eventTimeShort = "Feb. 24th - 26th"
+const eventTime = "February 24th - 26th, 2023"
+const description = `TurtleHacks is a hackathon happening from February 24th to 26th focused on the environment.`
 const image = {
     src: "https://i.imgur.com/z0ajy2J.png",
     type: "image/png",
@@ -11,7 +11,7 @@ const image = {
 
 const twitterCreator = "@TurtleHacks"
 const discordLink = "#"
-const linkedinLink = "#"
-const instagramLink = "#"
+const linkedinLink = "https://www.linkedin.com/company/turtlehacks/"
+const instagramLink = "https://www.instagram.com/turtle.hacks/"
 
 export { name, description, image, eventTime, eventTimeShort, twitterCreator, discordLink, linkedinLink, instagramLink }

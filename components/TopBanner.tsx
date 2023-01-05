de158cd14de48817ec9da0a7153237255bf2c42b
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2023-01-04 21:13:46 -0500

    feat: added banner with mentor & workshop apps

---
+++ components/TopBanner.tsx
@@ -0,0 +1,32 @@
+export default function TopBanner({ showBanner }: { showBanner: boolean }) {
+    return (
+        <div className={`flex items-center justify-center w-full p-2 md:px-8 bg-emerald-900 ${showBanner ? "min-h-12" : "h-0 hidden"} transition-all duration-300`}>
+            <p className="text-gray-200 text-center text-lg">
+                    <a 
+                        className="text-blue-400 hover:underline duration-150 font-medium"
+                        href="https://forms.gle/voiSye7MA4rhQwQe7"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Mentor
+                    </a>
+                {" "}
+                and
+                {" "}
+                    <a 
+                        className="text-blue-400 hover:underline duration-150 font-medium"
+                        href="https://forms.gle/yWdUy6LrGY5XQNv39"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Workshop Lead
+                    </a>
+                {" "}
+                applications are
+                {" "}
+                <b>open</b>!
+                Click the links for more info.
+            </p>
+        </div>
+    )
+}

6bfd9ab8033e9e422782d8d2b0376a4994fff935
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2023-04-10 14:49:53 -0400

    feat: finished and polished event details

--- components/Home/EventDetails.tsx
+++ components/Home/EventDetails.tsx
@@ -1,6 +1,8 @@
-import { IconType } from "react-icons";
 import { HiCalendar, HiLocationMarker, } from "react-icons/hi"
-import { MdMoneyOff, MdBackpack, MdPerson, MdFastfood } from "react-icons/md"
+import { MdPeopleAlt } from "react-icons/md"
+
+import fadeFrom, { fadeFromVariants, Direction } from "@util/anim/fadeFrom"
+import { m } from "framer-motion"
 
 export interface DetailCardData {
     title: string,
@@ -10,41 +12,33 @@ export interface DetailCardData {
 
 const cardData: DetailCardData[] = [
     {
-        title: "When",
-        icon: <HiCalendar className='text-blue-500' />,
-        content: "Friday December 16 4:30PM-7:30PM\nSaturday December 17 9:30AM-5:20PM"
+        title: "Type",
+        icon: <MdPeopleAlt className='text-emerald-600' />,
+        content: "Hybrid (in-person & online). View the FAQ for more information!"
     },
     {
         title: "Where",
-        icon: <HiLocationMarker className='text-blue-500' />,
-        content: "The Woodlands Secondary School, located at 3225 Erindale Station Road, Mississauga, Ontario."
+        icon: <HiLocationMarker className='text-emerald-600' />,
+        content: "Catalyst Commons @ 137 Glasgow St Unit 210, Kitchener, ON N2G 4X8"
     },
     {
-        title: "What to bring",
-        icon: <MdBackpack className='text-blue-500' />,
-        content: "Any tools you'll need for your project, and an open mind!"
-    },
-    {
-        title: "Experience required",
-        icon: <MdPerson className='text-blue-500' />,
-        content: "None! No tech experience is required to attend hack::peel"
-    },
-    {
-        title: "Cost",
-        icon: <MdMoneyOff className='text-blue-500' />,
-        content: "hack::peel is free for all attendees!"
-    },
-    {
-        title: "Food",
-        icon: <MdFastfood className='text-blue-500' />,
-        content: "All meals are provided for free!"
+        title: "When",
+        icon: <HiCalendar className='text-emerald-600' />,
+        content: "In-person: Sat, May 13th: 10AM to 4PM \nOnline: Fri, May 12th @ 5PM to Sun, May 14th @ 9AM"
     },
 ];
 
-function DetailCard(data: DetailCardData) {
+function DetailCard({ data, idx } : { data: DetailCardData, idx: number }) {
     return (
-        <div className="flex flex-col p-4 gap-4 items-center lg:items-start text-center lg:text-left">
-            <div className="flex items-center justify-center text-2xl text-white bg-blue-50 w-12 h-12 rounded-lg">
+        <m.div
+            className="flex flex-col p-4 gap-4 items-center lg:items-start text-center lg:text-left"
+            variants={fadeFromVariants(Direction.BOTTOM, 1, idx * 0.25, 30)}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+        >
+
+            <div className="flex items-center justify-center text-2xl text-white bg-green-50 w-12 h-12 rounded-lg">
                 {data.icon}
             </div>
 
@@ -52,31 +46,30 @@ function DetailCard(data: DetailCardData) {
             <p className="text-lg text-gray-300 whitespace-pre-line">
                 {data.content}
             </p>
-        </div>
+        </m.div>
     )
 }
 
 export default function EventDetails() {
     return (
-        <section className="flex flex-col items-center p-10 pt-16 md:p-20 md:py-16 md:pt-32 lg:px-32 lg:pb-24 lg:pt-32 xl:px-40 xl:pt-36 z-10 text-center bg-emerald-800" id="details">
+        <section className="flex flex-col items-center p-10 pt-16 md:p-20 md:py-16 md:pt-32 lg:pb-24 lg:pt-32 xl:pt-36 z-10 text-center bg-emerald-800" id="event-details">
             <div className="flex flex-col text-center mb-8">
                 <h1 className="text-4xl text-white font-bold">Event Overview</h1>
             </div>
 
-            <div className="flex flex-row justify-center items-center w-full mx-auto flex-wrap">
-                <div className="event-details-map-container" style={{ alignSelf: "center" }}>
-                    <div className="column">
-                        <div className="venue-map border-black-fade"
-                            style={{ borderColor: "#101A38", borderWidth: "5px", borderStyle: "solid" }}>
-                            <iframe
-                                src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyAWYhnX4mep0gM1pI9FQ2_kLk5rxYUBEzw&q=Catalyst+Commons"}
-                                style={{ border: "0", marginBottom: "-5px" }} allowFullScreen />
-                        </div>
-                    </div>
-                </div>
+            <div className="flex flex-row justify-center w-full h-full flex-wrap gap-6">
+                <m.iframe
+                    src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyAWYhnX4mep0gM1pI9FQ2_kLk5rxYUBEzw&q=Catalyst+Commons"}
+                    allowFullScreen
+                    className="w-full lg:w-1/3 h-auto m-4"
+                    variants={fadeFromVariants(Direction.LEFT, 1, 0, 60)}
+                    initial="initial"
+                    whileInView="animate"
+                    viewport={{ once: true }}
+                />
 
                 <div className="grid grid-cols-1 md:grid-cols-2 flex-wrap justify-between lg:w-1/2 h-full gap-4">
-                    {cardData.map(card_entry => (<DetailCard key={card_entry.title} {...card_entry} />))}
+                    {cardData.map((data, i) => (<DetailCard key={i} idx={i} data={data} />))}
                 </div>
             </div>
         </section>
